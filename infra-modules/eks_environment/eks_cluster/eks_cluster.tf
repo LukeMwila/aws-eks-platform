8389d75cac82d5db936bc75f621d38202fd84c96
Luke Mwila <outlier.developer@gmail.com>
2020-07-09 12:57:38 +0200

    feat: added data source to retrieve the root CA thumbprint

--- infra-modules/eks_environment/eks_cluster/eks_cluster.tf
+++ infra-modules/eks_environment/eks_cluster/eks_cluster.tf
@@ -17,8 +17,13 @@ resource "aws_eks_cluster" "main" {
   ]
 }
 
+### External cli kubergrunt
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.main.identity.0.oidc.0.issuer]
+}
+
 resource "aws_iam_openid_connect_provider" "main" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  thumbprint_list = [data.external.thumb.result.thumbprint]
   url             = "${aws_eks_cluster.main.identity.0.oidc.0.issuer}"
 }
